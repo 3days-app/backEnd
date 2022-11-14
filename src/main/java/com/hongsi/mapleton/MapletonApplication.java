cbdbd6173694d267af1f221fc126b91064dd4a41
seongwon-ha <djfqks22@naver.com>
2022-11-14 23:50:01 +0900

    시작

---
+++ src/main/java/com/hongsi/mapleton/MapletonApplication.java
@@ -0,0 +1,13 @@
+package com.hongsi.mapleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapletonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongsi/mapleton/MapletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongsi.mapleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
